98940d946c2babe7b627ca1c3e95de658597d9f5
Sam <sambisselele@gmail.com>
2022-11-22 11:33:06 +0100

    Feat : create new conversation

--- src/components/UserCard/userCard.js
+++ src/components/UserCard/userCard.js
@@ -1,21 +1,86 @@
+/* eslint-disable arrow-body-style */
+/* eslint-disable no-unused-vars */
+import { useDispatch } from "react-redux";
+import axios from "axios";
 
+import { getContactPerson } from "../../store/features/contactPerson/contactPersonSlice";
+import {
+  selectConversation,
+  getMessages,
+  // getAConversation,
+  getThisContactPerson,
+} from "../../store/features/conversations/conversationSlice";
 
-const UserCard = ({ props}) => {
+import { socket } from "../../socket";
+
+const userId = localStorage.getItem("userID");
+const token = localStorage.getItem("token");
+
+const UserCard = ({ props }) => {
+  const dispatch = useDispatch();
+
+  
+
+  let room = null;
+  const createNewConversation = () => {
+    axios({
+      method: "POST",
+      url: `http://localhost:8000/api/conversations/new/${userId}/${props._id}`,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `${token}`,
+      },
+    })
+      .then((response) => {
+        room = response.data.conversation._id;
+
+        console.log(response.data);
+
+        socket.emit("join", { userId, room });
+        dispatch(selectConversation(response.data.conversation._id));
+
+        dispatch(
+          getContactPerson(
+            response.data.conversation.participants
+              .filter((participant) => participant._id !== userId)
+              .map((user) => user)[0]
+          )
+        );
+        dispatch(getMessages(response.data.conversation.messages));
+        dispatch(
+          getThisContactPerson(
+            response.data.conversation.participants
+              .filter((participant) => participant._id !== userId)
+              .map((user) => user._id)[0]
+          )
+        );
+      })
+      .catch((error) => console.error(error));
+  };
+
+  
+
   return (
     <div
       className="user__list--card"
-    //   onClick={() => {
-    //     createNewConversation();
-    //   }}
+      onClick={() => {
+        createNewConversation();
+
+        dispatch(getThisContactPerson(props));
+        dispatch(getContactPerson(props));
+      }}
     >
       <div className="user__list--user__picture">
         <img src="/images/user.png" alt="profile pictur" />
       </div>
       <div className="user__list--user__message">
-        <h3> {props.firstName} {props.lastName}</h3>
+        <h3>
+          {" "}
+          {props.firstName} {props.lastName}
+        </h3>
       </div>
     </div>
   );
-}
+};
 
-export default UserCard
+export default UserCard;
